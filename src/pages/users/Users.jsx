fa31a8fc64490bbc3268278ceab5080094232648
danieljacquin <danielcamilo853@gmail.com>
2022-05-28 12:22:59 -0500

    usercss is deleted

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState, useRef, useContext } from 'react';
 import usersService from '../../services/users.service';
-import './users.css';
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
